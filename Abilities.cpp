3527b520442499ca26191f4286429deaa764ca93
Hettrick1 <evann.jacquot@gmail.com>
2024-02-23 14:12:34 +0100

    [Add] Abilities definition

--- Abilities.cpp
+++ Abilities.cpp
@@ -1,1 +1,33 @@
 #include "Abilities.h"
+
+Abilities::Abilities(std::string name, float damages, PokeType type) {
+	mName = name;
+	mDamages = damages;
+	mType = type;
+	mMaxUses = 5;
+	mCurrentUses = 0;
+}
+
+Abilities::~Abilities() {}
+
+float Abilities::GetDamages() {
+	return mDamages;
+}
+
+PokeType Abilities::GetAbilityType() {
+	return mType;
+}
+
+std::string Abilities::GetAbilityName() {
+	return mName;
+}
+
+bool Abilities::CanUse() {
+	return mCurrentUses < mMaxUses;
+}
+void Abilities::ResetUses() {
+	mCurrentUses = 0;
+}
+void Abilities::SetCurrentUses() {
+	mCurrentUses++;
+}
--- Abilities.h
+++ Abilities.h
@@ -1,5 +1,27 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+#include "Poketype.h"
+
 class Abilities
 {
+private:
+	std::string mName;
+	float mDamages;
+	int mMaxUses;
+	int mCurrentUses;
+	PokeType mType;
+
+public:
+	Abilities(std::string name, float damages, PokeType type);
+	~Abilities();
+	float GetDamages();
+	PokeType GetAbilityType();
+	std::string GetAbilityName();
+	bool CanUse();
+	void ResetUses();
+	void SetCurrentUses();
 };
 
